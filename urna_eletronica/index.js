6545f3ee619c67bb08e8acad516d82e36952651b
Leehaney Soares Ferreira Nazaré <leehaney.soares@gmail.com>
2022-07-21 10:09:33 -0300

    chore(urnaEletronica): removendo variável buttons e acessando pelo método de keyboard

--- urna_eletronica/index.js
+++ urna_eletronica/index.js
@@ -2,8 +2,7 @@ import Keyboard from './keyboard/Keyboard.js'
 
 const buttonsDiv = document.querySelector('.urna__buttons')
 const keyboard = Keyboard.create()
-const buttons = keyboard.buttons
 
-buttons.forEach(element =>
+keyboard.buttons.forEach(element =>
   buttonsDiv.appendChild(element.button)
 )
