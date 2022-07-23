10f4b3d5c0ed6c6c57e37f393cd00835f9d4dd0e
Leehaney Soares Ferreira Nazaré <leehaney.soares@gmail.com>
2022-07-23 18:16:50 -0300

    chore(urnaEletronica): criando métodos acessores para o display

--- urna_eletronica/display/display.js
+++ urna_eletronica/display/display.js
@@ -1,13 +1,53 @@
-import $ from '../shortcuts/$.js'
-
 class Display {
+  #aviso
+  #cargo
+  #descricao
+  #lateral
+  #numeros
   #title
 
-  get title () {
-    return this.#title ??= 
+  get aviso () {
+    return this.#aviso
   }
-  static create () {
-    return new Display()
+
+  get cargo () {
+    return this.#cargo
+  }
+
+  get descricao () {
+    return this.#descricao
+  }
+
+  get lateral () {
+    return this.#lateral
+  }
+
+  get numeros () {
+    return this.#numeros
+  }
+
+  get title () {
+    return this.#title
+  }
+
+  constructor (title, cargo, descricao, aviso, lateral, numeros) {
+    this.#title = title,
+    this.#cargo = cargo
+    this.#descricao = descricao
+    this.#aviso = aviso
+    this.#lateral = lateral
+    this.#numeros = numeros
+  }
+
+  static create (descriptor) {
+    return new Display(
+      descriptor?.title,
+      descriptor?.cargo,
+      descriptor?.descricao,
+      descriptor?.aviso,
+      descriptor?.lateral,
+      descriptor?.numeros
+    )
   }
 }
 
