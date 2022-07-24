3b96ee3fcd133d726f5bb7aa047cf695729e3c98
Leehaney Soares Ferreira Nazaré <leehaney.soares@gmail.com>
2022-07-24 18:54:23 -0300

    refactor(urnaEletronica): faz mais sentido criar uma instância de urna do que de display

--- urna_eletronica/index.js
+++ urna_eletronica/index.js
@@ -1,7 +1,3 @@
-import descriptor from './interface/descriptor.js'
-import Display from './display/display.js'
-import schema from './schema.js'
+import Urna from './Urna.js'
 
-
-const display = Display.create(descriptor)
-
+Urna.create()
